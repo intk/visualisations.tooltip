f8f5a901cb68b5d91a723088720b96b9e807aeb5
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2020-05-06 16:23:56 +0200

    Use input and font style from configuration panel

--- textboxSource.js
+++ textboxSource.js
@@ -1,9 +1,23 @@
 function drawTooltip(data) {
 
+  // Declare variable for tooltip text
+  let tooltipText = data.style.content.value || "";
+
+  // Create object where the text will be displayed
 	document.body.innerHTML = "";
 	var textElement = document.createElement('div');
 	textElement.id = 'tooltipText';
-	textElement.innerText = 'Test';
+	textElement.innerText = tooltipText;
+
+  // Get font colour from configuration
+  const textColor =  data.style.fontColour.value
+  ? data.style.fontColour.value.color
+  : data.style.fontColour.defaultValue;
+
+  // Set font style properties from configuration
+  textElement.style.color = textColor;
+  textElement.style.fontSize = data.style.fontSize.value+"px";
+  textElement.style.fontFamily = data.style.fontFamily.value;
 	document.body.appendChild(textElement);
 
 }
